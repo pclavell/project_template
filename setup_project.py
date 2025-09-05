85093e3c5d0374b3f5f5dcb97ce16ad7d5e3574f
fairliereese <fairliek@comcast.net>
2025-09-05 17:18:02 +0200

    added setup python script

--- setup_project.py
+++ setup_project.py
@@ -1,22 +1,72 @@
 from user1.resources.utils import *
+import pathlib
 
-# requires you to provide users, usernames, project name
-users = ['pclavell', 'freese']
-usernames = ['bsc083549', 'bsc083001']
-project_name = 'fetal_dev'
-
-# clones this dir
-cmd = 'git clone git@github.com:Mele-Lab/project_template.git'
+m = load_resources()
+m['path_map'] = {}
 
 # rename project; immediately remove all git things;
-cmd = 'cd project_template'
-cmd = 'rm -rf . git'
+cmd = 'rm -rf .git'
+run_cmd(cmd)
+cmd = f"mv ../project_template ../{m['setup_settings']['project_name']}"
+run_cmd(cmd)
 
-# adds entries to resources.yml for mn5
-m = load_resources()
+# adding path map entries for MN5 location
+
+# data dir, ref dir, and figures dir are shared by all users
+pref = f"{m['setup_settings']['mn5_projects']}/{m['setup_settings']['project_name']}"
+data_dir = f'{pref}/data/'
+ref_dir = f'{pref}/ref/'
+figures_dir = f'{pref}/figures/'
+
+def make_user_path_map_entry(username):
+    m['path_map'][username] = {}
+    m['path_map'][username]["\{data_dir\}"] = str(pathlib.Path(data_dir))
+    m['path_map'][username]["\{ref_dir\}"] = str(pathlib.Path(ref_dir))
+    m['path_map'][username]["\{figures_dir\}"] = str(pathlib.Path(figures_dir))
+
+    # metadata dir is part of the github-stored stuff, so it's separate
+    m['path_map'][username]["\{metadata_dir\}"] = str(pathlib.Path(f'{pref}/{user}/metadata/'))
+
+    return m
+
+
+# loop through usernames
+for user, entry in m['setup_settings']['users'].items():
+    m = make_user_path_map_entry(entry['mn5_username'])
+
+# also make one for temp mn5 user
+m = make_user_path_map_entry('template_mn5_user_do_not_remove')
+m['path_map']['template_mn5_user_do_not_remove']
+
+
+# adding path map entries for LOCAL location
+
+def make_user_path_map_entry_local(user, username):
+    # data dir, ref dir, and figures dir are shared by all users
+    pref = f"{m['setup_settings']['users'][user]['local_path']}/{m['setup_settings']['project_name']}/"
+    data_dir = f'{pref}/data/'
+    ref_dir = f'{pref}/ref/'
+    figures_dir = f'{pref}/figures/'
+
+    m['path_map'][username] = {}
+    m['path_map'][username]["\{data_dir\}"] = str(pathlib.Path(data_dir))
+    m['path_map'][username]["\{ref_dir\}"] = str(pathlib.Path(ref_dir))
+    m['path_map'][username]["\{figures_dir\}"] = str(pathlib.Path(figures_dir))
+    m['path_map'][username]["\{metadata_dir\}"] = str(pathlib.Path(f'{pref}/{user}/metadata/'))
+
+    return m
+
+# loop through usernames
+for user, entry in m['setup_settings']['users'].items():
+    m = make_user_path_map_entry_local(user, entry['local_username'])
+
+# write, just append the path_map
+m_append = {}
+m_append['path_map'] = m['path_map']
+with open('user1/resources/resources.yml', 'a') as f:
+        yaml.dump(m_append, f, default_flow_style=False)
 
 # make a copy of template user for each user
-for user in users:
-    cmd = f'cp user1/ {user}/'
-
-# run git init in the different user folders
+for user in m['setup_settings']['users']:
+    cmd = f'cp -r user1/ {user}'
+    run_cmd(cmd)

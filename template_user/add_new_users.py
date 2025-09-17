b2443e3856b0ca589ee4663508989962c6e60a68
fairliereese <fairliek@comcast.net>
2025-09-17 19:01:39 +0200

    hopefully fixed add_new_users; removed irrelvant info from resources.yml

--- template_user/add_new_users.py
+++ template_user/add_new_users.py
@@ -1,26 +1,24 @@
-################################## README BEFORE USAGE ##################################
+from pathlib import Path
+import re
+import os
+import sys
+import pandas as pd
+from snakemake.io import expand
 
-# Set up settings in resources.yml and in the project directory according to what's in resources.yml setup_settings; selectively add directories and init Git repos for only new users
+script_path = Path.cwd()
+user_dir = re.sub(r"/(metadata|processing|analysis)/.*$", "", str(script_path))
+sys.path.append(user_dir)
 
-# Usage: python add_new_users.py
+from resources.utils import *
 
-                                    #   /\_/\
-                                    #  ( o.o )
-                                    #   > ^ <
-                                    #  /     \
-                                    # (       )
-                                    #  \__ __/
-                                    #   || ||
-
-############ --------------------------------------------------------------- ############
-
-from template_user.resources.utils import *
-import pathlib
+# user carrying out the user addition
+curr_user = str(Path(user_dir).name)
 
 m = load_resources()
 
 # list of new users
-new_users = list(set(list(m['setup_settings']['users'].keys()))-set(m['users']))
+curr_users = m['users']
+new_users = list(set(list(m['setup_settings']['users'].keys()))-set(curr_users))
 
 if len(new_users) == 0: 
     raise ValueError('No new users found. Exiting.')
@@ -39,25 +37,48 @@ quick_path_map['template_user'] = get_user_system_entry_path_map(m, 'template_us
 # also add a users list 
 quick_path_map['users'] = list(m['setup_settings']['users'].keys())
 
-# when writing, we now need to overwrite previous entries
-path_map = {'path_map': dict(path_map)}
-m['path_map'] = path_map
+# path_map from default dict to normal dict (defaultdict
+# doesn't save nicely in yaml)
+path_map =  dict(path_map)
 
-# quick path map
-for key, item in quick_path_map.items():
-    m[key] = item
-
-# rewrite
-with open('template_user/resources/resources.yml', 'w') as f:
-    yaml.dump(m, f, default_flow_style=False)
+# for each current user, update the resources.yml
+for user in curr_users:
+    print(user)
     
-# which one was the new user ? copy a preexisting user for them
-# idea is to get also the git info 
+    # load this users' resources to add the new user to
+    user_cfg = f'../{user}/resources/resources.yml'
+    print(user_cfg)
+    m = load_config(user_cfg)
+    print()
+    
+    # when writing, we now need to overwrite previous entries
+    m['path_map'] = path_map
+
+    # quick path map
+    for key, item in quick_path_map.items():
+        m[key] = item
+        
+    # rewrite
+    with open(user_cfg, 'w') as f:
+        yaml.dump(m, f, default_flow_style=False)
+
+# for each new user, copy the user's directory that is carrying out 
+# the change, and switch to the main branch
+curr_user_dir = str(Path(user_dir))
 for user in new_users:
-    cmd = f"cp -r {list(m['setup_settings']['users'].keys())[0]} {user}"
-    # print(cmd)
+    new_user_dir = str(Path(f'../{user}').resolve())
+    
+    cmd = f"cp -r {curr_user_dir} {new_user_dir}"
+    print(cmd)
     run_cmd(cmd)
     cmd = "git fetch origin"
-    run_cmd(cmd, wd=user)
+    print(cmd)
+    run_cmd(cmd, wd=new_user_dir)
     cmd = "git reset --hard origin/main"
-    run_cmd(cmd, wd=user)
+    print(cmd)
+    run_cmd(cmd, wd=new_user_dir)
+    cmd = "git checkout main"
+    print(cmd)
+    run_cmd(cmd, wd=new_user_dir)
+    
+    

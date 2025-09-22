a72ab594e6afb74574b1774184288845386bd99a
pclavell <pauclavellrevelles@gmail.com>
2025-09-22 20:42:20 +0200

    improve bash header

--- template_user/processing/template.sh
+++ template_user/processing/template.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
-USERPATH=realpath $(dirname "${BASH_SOURCE[0]}") | sed -E 's#/metadata(/|$)|/processing(/|$)|/analysis(/.*|$)##g'
-cd $USERPATH
-# use always relative paths to your user (absolute paths won't work for other users because they have another user_dir)
+# README
+#      1) User either config references to files or paths relative to your user
+cd $(realpath $(dirname "${BASH_SOURCE[0]}") | sed -E 's#/metadata(/|$)|/processing(/|$)|/analysis(/.*|$)##g')
+CONFIGFILE_MN5="./resources/config_mn5.yml"
+
+
+# example to use yaml parser
+MYFILE=$(yq eval '.data.sam' $CONFIGFILE_MN5)
+echo $MYFILE
+

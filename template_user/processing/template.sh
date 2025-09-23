350bf1030564b89d501813be076c6886f123e006
pclavell <pauclavellrevelles@gmail.com>
2025-09-23 14:10:38 +0200

    add module load yq

--- template_user/processing/template.sh
+++ template_user/processing/template.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # README
 #      1) User either config references to files or paths relative to your user
+module load yq
 cd $(realpath $(dirname "${BASH_SOURCE[0]}") | sed -E 's#/metadata(/|$)|/processing(/|$)|/analysis(/.*|$)##g')
 CONFIGFILE_MN5="./resources/config_mn5.yml"
 

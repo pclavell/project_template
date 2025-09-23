433bb3b9d2861326500f84c9b8101b74c97c8136
pclavell <pauclavellrevelles@gmail.com>
2025-09-23 12:31:46 +0200

    debug here in template.R

--- template_user/analysis/template.R
+++ template_user/analysis/template.R
@@ -1,5 +1,10 @@
 # HEADER -----------------------------------------------------------------------
+script_dir <- dirname(ifelse(length(commandArgs(trailingOnly = F))>2,normalizePath(gsub("--file=","", commandArgs(trailingOnly = F)[4])),
+                              normalizePath(rstudioapi::getActiveDocumentContext()$path)))
+script_dir <- dirname(ifelse(script_dir == "IRkernel::main()",normalizePath(getwd()), script_dir))
+setwd(script_dir)
 library(here)
+setwd(here())
 source(here("resources", "utils.r")) # here referes to where .here file sits (in the user dir)
 config <- load_config(here()) # to refer to files in config use config$
 paths <- load_paths(here()) # to refer to data, figs, metadata, ref, scratch use resources$

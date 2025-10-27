1fc71b714234f183cdde377c9cb66230e3c9c3e4
Pau Clavell-Revelles <99911796+pclavell@users.noreply.github.com>
2025-10-27 10:43:46 +0100

    Update template.R header

--- template_user/analysis/template.R
+++ template_user/analysis/template.R
@@ -1,11 +1,13 @@
-# HEADER -----------------------------------------------------------------------
-script_dir <- normalizePath(dirname(ifelse(interactive(),rstudioapi::getActiveDocumentContext()$path,
-                                           gsub("--file=","", commandArgs(trailingOnly = F)[4]))))
-script_dir <- normalizePath(ifelse(script_dir == "IRkernel::main()",getwd(), script_dir))
-setwd(script_dir)
+# HEADER ------------------------------------------------------------------------
+if(interactive() & !rstudioapi::isAvailable()){setwd(getwd()) # WARNING: only works if run from Project_Dir/User_dir !!
+}else{
+  script_dir <- normalizePath(dirname(ifelse(interactive(), rstudioapi::getActiveDocumentContext()$path,
+                                            gsub("--file=", "", commandArgs(trailingOnly = F)[4]))))
+  script_dir <- normalizePath(ifelse(script_dir == "IRkernel::main()", getwd(), script_dir))
+  setwd(script_dir)}
 library(here)
 setwd(here())
-source(here("resources", "utils.r")) # here refers to where .here file sits (in the user dir)
+source(here("resources", "utils.r")) # here referes to where .here file sits (in the user dir)
 config <- load_config() # to refer to files in config use config$
 paths <- load_paths() # to refer to data, figs, metadata, ref, scratch use resources$
 #--------------------------------------------------------------------------------

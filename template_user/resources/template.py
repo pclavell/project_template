bcb67e76514203371b4b9827fe3ae45d332abaac
Pau Clavell-Revelles <99911796+pclavell@users.noreply.github.com>
2025-12-10 15:49:24 +0100

    Update template.py

--- template_user/resources/template.py
+++ template_user/resources/template.py
@@ -2,7 +2,7 @@
 from pyprojroot.here import here
 import sys
 sys.path.append(str(here()))
-from resources.utils import *
+from resources.utils import * # if the ModuleNotFound in mn5 it is probably because of the module you used. Use module load anaconda
 config = load_config() # to refer to files in config use config$
 paths = load_paths() # to refer to data, figs, metadata, ref, scratch use resulting paths$
 #--------------------------------------------------------------------------------
